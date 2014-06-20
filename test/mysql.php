e7821575e7fb27c9c7d326215fa7321045725a82
Ry Ferguson <rytf12@gmail.com>
2014-06-20 16:36:40 -0700

    add MySQL tests

---
+++ test/mysql.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+
+//connect to db
+$db = new pjsql\Mysql(
+    'host',
+    'username',
+    'password',
+    'db');
+
+//exec() executes a result-less query
+$db->exec('CREATE TABLE tword (
+    word_id INT AUTO_INCREMENT PRIMARY KEY,
+    word VARCHAR(32))');
+
+$db->exec('INSERT INTO tword (word)
+    VALUES("was"), ("jump"), ("hello")');
+
+//escape strings with esc()
+$db->exec(sprintf('INSERT INTO tword (word) VALUES("%s")',
+    $db->esc('&><\/"')));
+
+//query() returns a 2d array of results
+var_dump($db->query('SELECT * FROM tword'));
