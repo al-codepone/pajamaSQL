000cb1d45d5a1bf758614fc1d044b4aaae8660f2
Ry Ferguson <rytf12@gmail.com>
2017-11-22 13:15:04 -0800

    change SQL whitespace for the MySQL test

--- test/mysql.php
+++ test/mysql.php
@@ -13,16 +13,30 @@ $db = new pjsql\Mysql(
     'db');
 
 //exec() executes a result-less query
-$db->exec('CREATE TABLE tword (
-    word_id INT AUTO_INCREMENT PRIMARY KEY,
-    word VARCHAR(32))');
+$db->exec('
+    CREATE TABLE tword(
+        word_id INT AUTO_INCREMENT PRIMARY KEY,
+        word VARCHAR(32))');
 
-$db->exec('INSERT INTO tword (word)
-    VALUES("was"), ("jump"), ("hello")');
+$db->exec('
+    INSERT INTO
+        tword(word)
+    VALUES
+        ("was"),
+        ("jump"),
+        ("hello")');
 
 //escape strings with esc()
-$db->exec(sprintf('INSERT INTO tword (word) VALUES("%s")',
+$db->exec(sprintf('
+    INSERT INTO
+        tword(word)
+    VALUES
+        ("%s")',
     $db->esc('&><\/"')));
 
 //query() returns a 2d array of results
-var_dump($db->query('SELECT * FROM tword'));
+var_dump($db->query('
+    SELECT
+        *
+    FROM
+        tword'));
